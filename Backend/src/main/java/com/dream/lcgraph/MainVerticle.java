f03fe15a6d7fcf3129b86a7a1b8d186ae69fc31a
David Zhou <zhouyi021022@gmail.com>
2023-01-14 21:40:52 +1100

    Add backend

--- Backend/src/main/java/com/dream/lcgraph/MainVerticle.java
+++ Backend/src/main/java/com/dream/lcgraph/MainVerticle.java
@@ -1,23 +1,41 @@
 package com.dream.lcgraph;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+    // Scale application
+    DeploymentOptions opts = new DeploymentOptions()
+                              .setWorker(true)
+                              .setInstances(8);
+
+    // Register other verticle
+    vertx.deployVerticle("com.dream.lcgraph.HelloVertice", opts);
+
+    // Similar to Express, gives capability to give handeler to different router
+    Router router = Router.router(vertx);
+    router.get("/api/v1/hello").handler(this::helloVertx);
+    router.get("/api/v1/nope").handler(this::helloName);
+
+    vertx.createHttpServer().requestHandler(router).listen(8080);
+  }
+
+  void helloVertx(RoutingContext ctx) {
+    vertx.eventBus().request("hello.vertx.addr", "", reply -> {
+      ctx.request().response().end((String) reply.result().body());
+    });
+  }
+
+  void helloName(RoutingContext ctx) {
+    String name = ctx.pathParam("name");
+    vertx.eventBus().request("hello.named.addr", name, reply -> {
+      ctx.request().response().end((String) reply.result().body());
     });
   }
 }
